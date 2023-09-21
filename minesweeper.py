8e815ceaa82e60b712d23a4e67f12a79d86ca012
Nigel-Harvey <nigelharvey15@gmail.com>
2023-09-21 16:02:04 -0400

    Add python file. No data yet

---
+++ minesweeper.py
@@ -0,0 +1,7 @@
+# Auther:     Nigel Harvey
+# Purpose:    Create a window in which the user can play minesweeper using their mouse to click ad navigate the GUI
+# Files:      <art files: bomb, flag>, <structurtal files>
+
+
+import pygame
+
