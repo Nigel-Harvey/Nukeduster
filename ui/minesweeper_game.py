e1f94a232ef1b8d5ad611b3b557975d4047ea14c
Nigel-Harvey <nigelharvey15@gmail.com>
2023-09-21 16:29:26 -0400

    Added psuedocode comments to structrue the UI

--- ui/minesweeper_game.py
+++ ui/minesweeper_game.py
@@ -4,3 +4,28 @@
 
 import pygame
 
+
+# open game window
+
+
+# let user input name and select difficulty
+
+
+# set new window based on difficulty chosen
+
+
+# load in game data and set clock to 0
+
+
+# wait for first click, then start the clock
+
+
+# loop through game logic until "Game Over" through either winning or losing
+
+
+# show screen with option to continue, which doesn't impede sight of the game
+
+
+# once continue is chosen, show scoreboard and leave buttons to exit or play again
+
+
