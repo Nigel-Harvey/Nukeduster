b21c1d7b2e44eb764944f6b99a47baef697a688f
Nigel-Harvey <nigelharvey15@gmail.com>
2023-10-04 22:08:46 -0400

    Added new constants to compliment new features

--- data/constants.py
+++ data/constants.py
@@ -1,8 +1,19 @@
 # Colours
-WHITE =     (255, 255, 255)
-BLACK =     (0, 0, 0)
-GREY =      (200, 200, 200)
-PURPLE =    (83, 20, 120)
+WHITE =         (255, 255, 255)
+BLACK =         (0, 0, 0)
+GREY_DARK =     (100, 100, 100)
+GREY =          (150, 150, 150)
+GREY_LIGHT =    (200, 200, 200)
+
+PURPLE =        (83, 20, 120)
+RED =           (255, 0, 0)
+ORANGE =        (255, 128, 0)
+BLUE =          (255, 0, 0)
+BLUE_LIGHT =    (255, 102, 102)
+YELLOW =        (255, 255, 0)
+GREEN =         (0, 255, 0)
+GREEN_DARK =    (0, 102, 0)
+PINK =          (255, 0, 255)
 
 # Screen sizes
 MENU_WIDTH =    600
@@ -19,3 +30,21 @@ MENU =      0
 EASY =      1
 MEDIUM =    2
 HARD =      3
+
+# Grid sizes (number of tiles)
+EASY_GRID_WIDTH =       9
+EASY_GRID_LENGTH =      9
+MEDIUM_GRID_WIDTH =     16
+MEDIUM_GRID_LENGTH =    16
+EXPERT_GRID_WIDTH =     30
+EXPERT_GRID_LENGTH =    16
+
+# Number of nukes
+EASY_NUKES =    10
+MEDIUM_NUKES =  40
+HARD_NUKES =    100
+
+# Game states
+STARTING =      0
+IN_PROGRESS =   1
+OVER =          2
